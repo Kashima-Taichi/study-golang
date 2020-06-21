cd4489c7f0a15f65561d0e26c2befe6336f31e14
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-06-21 17:13:35 +0900

    演習

---
+++ test/test.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Q1
+	f := 1.11
+	fmt.Println(int(f))
+
+	// Q2 5,6
+
+	m := map[string]int{"Mike": 20, "Nacy": 24, "Messi": 30}
+	fmt.Println(m)
+
+}
